35b4fe1fba426b59f712a214b649fa30a7be0811
Rachel Green <reg246@cornell.edu>
2020-07-13 15:08:24 -0400

    basic testing
    created 4 basic tests that achieve: basic, advanced, and denied coverage

--- src/main/java/com/sample/AppTest.java
+++ src/main/java/com/sample/AppTest.java
@@ -15,8 +15,14 @@ public class AppTest {
 			KieSession kSession= kContainer.newKieSession("ksession-rules");
 
 			// assembling facts and firing
-			Applicant A= new Applicant("Jane Doe", 1, "7/13/1990", 22124, true, Applicant.BACHELORS);
+			Applicant A= new Applicant("Jane Doe", 1, "7/13/1990", 5000, true, Applicant.MASTERS);
+			Applicant B= new Applicant("John Doe", 0, "7/15/2000", 95000, true, Applicant.HIGH_SCHOOL_DIPLOMA);
+			Applicant C= new Applicant("Rachel Green", 1, "12/10/1999", 22124, true, Applicant.BACHELORS);
+			Applicant D= new Applicant("Rachel Brown", 1, "12/15/1999", 50004, false, Applicant.HIGH_SCHOOL_DIPLOMA);
 			kSession.insert(A);
+			kSession.insert(B);
+			kSession.insert(C);
+			kSession.insert(D);
 			kSession.fireAllRules();
 		} catch (Throwable t) {
 			t.printStackTrace();
